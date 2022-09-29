303c7881b2f1cf240a570a6658b21fe798ffb4f7
francisco cataldo <franciscolork@gmail.com>
2022-09-29 18:22:49 -0300

    change bucket image to s3

--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,39 @@
 module.exports = ({ env }) => ({
     // ...
+    // upload: {
+    //   config: {
+    //     provider: 'aws-s3',
+    //     providerOptions: {
+    //       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //       secretAccessKey: process.env.AWS_ACCESS_SECRET,
+    //       region: process.env.AWS_REGION,
+    //       params: {
+    //         Bucket: process.env.AWS_BUCKET,
+    //       },
+    //     },
+    //     actionOptions: {
+    //       upload: {},
+    //       uploadStream: {},
+    //       delete: {},
+    //     },
+    //   },
+    // },
+
     upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          accessKeyId: env(process.env.AWS_ACCESS_KEY_ID),
-          secretAccessKey: env(process.env.AWS_ACCESS_SECRET),
-          region: env(process.env.AWS_REGION),
-          params: {
-            Bucket: env(process.env.AWS_BUCKET),
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_ACCESS_SECRET,
+            region: process.env.AWS_REGION,
+            params: {
+                Bucket: process.env.AWS_BUCKET,
+             },
+            endpoint:process.env.ENDPOINT, //workings
+            baseUrl: process.env.BASE_URL,
           },
         },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
       },
-    },
     // ...
   });
 
